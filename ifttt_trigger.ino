0b3063beb5a99e0ae0c00c606d803065f471e2af
Ankush4864 <132539800+Ankush4864@users.noreply.github.com>
2023-09-14 02:23:49 +0530

    Update ifttt_trigger.ino

--- ifttt_trigger.ino
+++ ifttt_trigger.ino
@@ -11,7 +11,7 @@ WiFiClient client;
 BH1750 lightMeter;
 
 char   HOST_NAME[] = "maker.ifttt.com";
-String PATH_NAME  = "/trigger/lightsensor/with/key/djmTxByajgaXmwohDns7rvPK_Nsk553vDqSaO1saWzK";
+String PATH_NAME  = "https://maker.ifttt.com/trigger/lightsensor/with/key/djmTxByajgaXmwohDns7rvPK_Nsk553vDqSaO1saWzK";
 String queryString = "?value1=57&value2=25";
 
 void setup() {
@@ -44,26 +44,32 @@ void loop() {
   Serial.print("START");
   float lux = lightMeter.readLightLevel();
 
-  // Serial.println("Light sensor: ");
-  // Serial.p'int(lux);
+  // Update the queryString with the current lux value and a descriptive message
+  if (lux > 1000) {
+    queryString = "?value1=" + String(lux) + "&value2=It's very bright!";
+  } else if (lux > 500) {
+    queryString = "?value1=" + String(lux) + "&value2=It's quite bright.";
+  } else if (lux > 100) {
+    queryString = "?value1=" + String(lux) + "&value2=It's somewhat dim.";
+  } else if (lux > 10) {
+    queryString = "?value1=" + String(lux) + "&value2=It's quite dark.";
+  } else {
+    queryString = "?value1=" + String(lux) + "&value2=It's very dark!";
+  }
 
-  queryString +="?value1=" ;
-  queryString += lux;
   Serial.println(queryString);
 
   if (lux > 500) {
-
-    // make a HTTP request:
+    // make an HTTP request:
     // send HTTP header
     client.println("GET " + PATH_NAME + queryString + " HTTP/1.1");
     client.println("Host: " + String(HOST_NAME));
     client.println("Connection: close");
     client.println(); // end HTTP header
 
-
     while (client.connected()) {
       if (client.available()) {
-        // read an incoming byte from the server and print it to serial monitor:
+        // read an incoming byte from the server and print it to the serial monitor:
         char c = client.read();
         Serial.print(c);
       }
@@ -72,8 +78,9 @@ void loop() {
     // the server's disconnected, stop the client:
     client.stop();
     Serial.println();
-    Serial.println("disconnected");
+    Serial.println("Disconnected");
   }
+  
   queryString = "";
   delay(3000);
 }
